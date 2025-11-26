7dbec903025e2fdb34ca9e643a0087a7b3cb1623
TarasovVP <vptarasov1998@gmail.com>
2025-11-26 19:05:32 +0200

    Minor improve

--- lib/features/users/data/users_api.g.dart
+++ lib/features/users/data/users_api.g.dart
@@ -9,11 +9,7 @@ part of 'users_api.dart';
 // ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element
 
 class _UsersApi implements UsersApi {
-  _UsersApi(
-    this._dio, {
-    this.baseUrl,
-    this.errorLogger,
-  }) {
+  _UsersApi(this._dio, {this.baseUrl, this.errorLogger}) {
     baseUrl ??= 'https://dummyjson.com/';
   }
 
@@ -29,22 +25,16 @@ class _UsersApi implements UsersApi {
     final queryParameters = <String, dynamic>{};
     final _headers = <String, dynamic>{};
     const Map<String, dynamic>? _data = null;
-    final _options = _setStreamType<UsersResponse>(Options(
-      method: 'GET',
-      headers: _headers,
-      extra: _extra,
-    )
-        .compose(
-          _dio.options,
-          Constants.users,
-          queryParameters: queryParameters,
-          data: _data,
-        )
-        .copyWith(
-            baseUrl: _combineBaseUrls(
-          _dio.options.baseUrl,
-          baseUrl,
-        )));
+    final _options = _setStreamType<UsersResponse>(
+      Options(method: 'GET', headers: _headers, extra: _extra)
+          .compose(
+            _dio.options,
+            Constants.users,
+            queryParameters: queryParameters,
+            data: _data,
+          )
+          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+    );
     final _result = await _dio.fetch<Map<String, dynamic>>(_options);
     late UsersResponse _value;
     try {
@@ -69,10 +59,7 @@ class _UsersApi implements UsersApi {
     return requestOptions;
   }
 
-  String _combineBaseUrls(
-    String dioBaseUrl,
-    String? baseUrl,
-  ) {
+  String _combineBaseUrls(String dioBaseUrl, String? baseUrl) {
     if (baseUrl == null || baseUrl.trim().isEmpty) {
       return dioBaseUrl;
     }
